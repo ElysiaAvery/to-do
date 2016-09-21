23789e5939ff8be1d7147958b0b7bf0e32e53280
Elysia Nason and Sandro Alvarez <elysia@>
2016-09-21 08:51:56 -0700

    add update and delete functionality

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -5,20 +5,8 @@ import static org.junit.Assert.*;
 
 public class CategoryTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
-  }
-
-  @After
-  public void tearDown() {
-    try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM tasks *;";
-      String deleteCategoriesQuery = "DELETE FROM categories *;";
-      con.createQuery(deleteTasksQuery).executeUpdate();
-      con.createQuery(deleteCategoriesQuery).executeUpdate();
-    }
-  }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void category_instantiatesCorrectly_true() {
@@ -112,4 +100,12 @@ public class CategoryTest {
     assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
   }
 
+  @Test
+  public void delete_deletesCategory_true() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    int myCategoryId = myCategory.getId();
+    myCategory.delete();
+    assertEquals(null, Category.find(myCategoryId));
+  }
 }
